2e1d3dbd51eaafce69a47a031df1ac7dc8e09186
Ignacio Jorquera <ijorquera@alumnos.uai.cl>
2024-05-31 11:15:56 -0400

    Modificación de config.py

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 CATEGORIES = {
-    "Books": [".epub"],
-    "Documents": [".pdf", ".docx", ".txt"],
+    "Documents": [".pdf", ".docx", ".txt", ".epub"],
     "Images": [".jpg", ".jpeg", ".png", ".gif", ".HEIC", ".heic", ".webp"],
     "Screenshots": [".png"],
     "Music": [".mp3", ".wav"],
     "Videos": [".mp4", ".mov"],
     "PPTs": [".ppt", ".pptx", ".odp"],
-    "Excels": [".xls", ".xlsx", ".ods"],
+    "Excels": [".xls", ".xlsx", ".ods", ".csv"],
     "Codes": [".py", ".js", ".html", ".css", ".java", ".php", ".ts", ".swift", ".sql", ".db"],
     "Archives": [".zip", ".rar", ".7z"]
 }
